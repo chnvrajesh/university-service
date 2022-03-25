5ae422c9241c041156aab3ab6d06f82a80c989a2
chnvrajesh <51875079+chnvrajesh@users.noreply.github.com>
2022-03-25 14:39:28 +0530

    university service

---
+++ src/test/java/com/nvrviews/universityservice/UniversityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvrviews.universityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
